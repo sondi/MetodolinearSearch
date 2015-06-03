5c74b3da19a51827cc6368449ce82ea645648c6f
codeamac02 <codeamac02@gmail.com>
2015-06-03 18:40:56 -0500

    Global Linear Search

--- LinearSearch.rb
+++ LinearSearch.rb
@@ -15,5 +15,23 @@ def linear_search(value,array)
 end
 
 random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
-puts linear_search(4, random_numbers)== 0
+puts linear_search(20, random_numbers)== 3
 puts linear_search(29, random_numbers)== nil
+
+def global_linear_search(value,array)
+	indices = []
+	posicion = 0
+	for i in array
+		if array[posicion] == value
+		   indices << posicion
+		end
+		posicion += 1
+	end
+	p indices
+end
+
+arr = "entretenerse".split('') 
+p arr
+p global_linear_search("e", arr) == [0, 4, 6, 8, 11]
+p global_linear_search("z", arr) == []
+# ["e", "n", "t", "r", "e", "t", "e", "n", "e", "r", "s", "e"]
